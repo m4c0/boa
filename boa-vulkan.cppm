37a49258c467567c1795d714360bec32fc6e9c72
Eduardo Costa <m4c0@users.noreply.github.com>
2023-02-20 12:26:00 +0000

    moving end_rp to module

--- boa-vulkan.cppm
+++ boa-vulkan.cppm
@@ -120,10 +120,12 @@ public:
     return *rnd_finished_sema;
   }
 
-  [[nodiscard]] auto wait_and_takeoff(const per_extent *ext) const {
+  [[nodiscard]] auto wait_and_takeoff(const per_extent *ext, auto fn) const {
     vee::wait_and_reset_fence(*f);
     auto idx = vee::acquire_next_image(ext->swapchain(), *img_available_sema);
     ext->begin_secondary_cmdbuf(cb);
+    fn(cb);
+    vee::end_cmd_buf(cb);
     return idx;
   }
 
--- main.cpp
+++ main.cpp
@@ -68,11 +68,8 @@ extern "C" void casein_handle(const casein::event &e) {
       try {
         auto &inf = infs->flip();
 
-        auto idx = inf.wait_and_takeoff(&*ext);
-        {
-          vee::cmd_draw(inf.command_buffer(), 3);
-          vee::end_cmd_buf(inf.command_buffer());
-        }
+        auto idx =
+            inf.wait_and_takeoff(&*ext, [](auto cb) { vee::cmd_draw(cb, 3); });
 
         inf.submit(&*dev, (*frms)[idx]->one_time_submit([&inf](auto cb) {
           vee::cmd_execute_command(cb, inf.command_buffer());
