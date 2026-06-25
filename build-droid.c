42611cba94b8f9267bf8c6b9d67090723e6252e7
Eduardo Costa <m4c0@users.noreply.github.com>
2026-06-25 22:55:37 +0100

    droid: reusing 'build.h'

--- build-droid.c
+++ build-droid.c
@@ -1,81 +1,54 @@
-#include <assert.h>
-#include <stdio.h>
-#include <stdlib.h>
+#include "build.h"
+
 #include <sys/stat.h>
-#include <unistd.h>
-
-static int run(char ** args) {
-  assert(args && args[0]);
-
-  pid_t pid = fork();
-  if (pid == 0) {
-    execvp(args[0], args);
-    abort();
-  } else if (pid > 0) {
-    int sl = 0;
-    assert(0 <= waitpid(pid, &sl, 0));
-    if (WIFEXITED(sl)) return WEXITSTATUS(sl);
-  }
-
-  fprintf(stderr, "failed to run child process: %s\n", args[0]);
-  return 1;
-}
 
 static int shader(char * name) {
   char spv[1024];
   sprintf(spv, "droid/%s.spv", name);
-
-  char * args[] = { "glslang", "-V", name, "-o", spv, 0 };
-  return run(args);
+  RUN("glslang", "-V", name, "-o", spv);
+  return 0;
 }
 
 #ifdef ARCH
 #define OBJ(x) ("droid/" ARCH "/" x)
 
 static int cc(char * src, char * o) {
-  char * args[] = {
-    "clang", "-Wall", "-g",
-    "-fdata-sections", "-ffunction-sections", "-funwind-tables",
-    "-fstack-protector-strong", "-no-canonical-prefixes",
-    "--target=" ARCH,
-    "--sysroot", ANDROID_NDK_PREBUILT_ROOT "/sysroot/",
-    "-IVulkan-Headers/include",
-    "-o", o, "-c", src, 0 };
-  return run(args);
+  CC(src, o,
+      "-fdata-sections", "-ffunction-sections", "-funwind-tables",
+      "-fstack-protector-strong", "-no-canonical-prefixes",
+      "--target=" ARCH,
+      "--sysroot", ANDROID_NDK_PREBUILT_ROOT "/sysroot/",
+      "-IVulkan-Headers/include");
+  return 0;
 }
-#define CC(src, o) cc(src, "droid/" ARCH "/" o)
+#define CC1(src, o) cc(src, "droid/" ARCH "/" o)
 
 static int hdr(char * src, char * o, char * d) {
-  char * args[] = {
-    "clang", "-Wall", "-x", "c", "-g",
-    "-fdata-sections", "-ffunction-sections", "-funwind-tables",
-    "-fstack-protector-strong", "-no-canonical-prefixes",
-    "--target=" ARCH,
-    "--sysroot", ANDROID_NDK_PREBUILT_ROOT "/sysroot/",
-    "-D", d, "-o", o, "-c", src,
-    0
-  };
-  return run(args);
+  HDR(src, o,
+      "-fdata-sections", "-ffunction-sections", "-funwind-tables",
+      "-fstack-protector-strong", "-no-canonical-prefixes",
+      "--target=" ARCH,
+      "--sysroot", ANDROID_NDK_PREBUILT_ROOT "/sysroot/",
+      "-D", d);
+  return 0;
 }
-#define HDR(src, o, d) hdr(src, OBJ(o), d)
+#define HDR1(src, o, d) hdr(src, OBJ(o), d)
 
 static int link_exe() {
-  char * args[] = {
-    "clang", "-Wall", "-shared",
-    "-Wl,-Bsymbolic", "-fuse-ld=lld", "-Wl,--no-undefined",
-    "-resource-dir", ANDROID_NDK_PREBUILT_ROOT "/lib/clang/21",
-    "--target=" ARCH,
-    "--sysroot", ANDROID_NDK_PREBUILT_ROOT "/sysroot/",
-    "-o", "droid/" ARCH "/libboas.so", 
-    OBJ("gme.o"),
-    OBJ("sfx.o"),
-    OBJ("snd.o"),
-    OBJ("snk.o"),
-    OBJ("tmr.o"),
-    OBJ("vulkan.o"),
-    OBJ("vulkan-droid.o"),
-    0 };
-  return run(args);
+  RUN("clang", "-Wall", "-shared",
+      "-Wl,-Bsymbolic", "-fuse-ld=lld", "-Wl,--no-undefined",
+      "-resource-dir", ANDROID_NDK_PREBUILT_ROOT "/lib/clang/21",
+      "--target=" ARCH,
+      "--sysroot", ANDROID_NDK_PREBUILT_ROOT "/sysroot/",
+      "-o", "droid/" ARCH "/libboas.so", 
+      OBJ("gme.o"),
+      OBJ("sfx.o"),
+      OBJ("snd.o"),
+      OBJ("snk.o"),
+      OBJ("tmr.o"),
+      OBJ("vulkan.o"),
+      OBJ("vulkan-droid.o"));
+  return 0;
 }
 #endif
 
@@ -92,11 +65,9 @@ static int meta(char * tgt) {
   char n[1024];
   snprintf(n, 1024, "-DANDROID_NDK_PREBUILT_ROOT=\"%s\"", ndk);
 
-  { char * args[] = { "clang", n, d, "-o", o, "build-droid.c", 0 };
-    if (run(args)) return 1; }
-
-  char * args[] = { o, 0 };
-  return run(args);
+  RUN("clang", n, d, "-o", o, "build-droid.c");
+  RUN(o);
+  return 0;
 }
 
 int main(int argc, char ** argv) {
@@ -114,14 +85,14 @@ int main(int argc, char ** argv) {
 #else
   mkdir("droid/" ARCH, 0777);
 
-  if (CC("vulkan.c", "vulkan.o")) return 1;
-  if (CC("vulkan-droid.c", "vulkan-droid.o")) return 1;
+  if (CC1("vulkan.c", "vulkan.o")) return 1;
+  if (CC1("vulkan-droid.c", "vulkan-droid.o")) return 1;
 
-  if (HDR("gme.h", "gme.o", "GME_IMPLEMENTATION")) return 1;
-  if (HDR("sfx.h", "sfx.o", "SFX_IMPLEMENTATION")) return 1;
-  if (HDR("snd.h", "snd.o", "SND_IMPLEMENTATION")) return 1;
-  if (HDR("snk.h", "snk.o", "SNK_IMPLEMENTATION")) return 1;
-  if (HDR("tmr.h", "tmr.o", "TMR_IMPLEMENTATION")) return 1;
+  if (HDR1("gme.h", "gme.o", "GME_IMPLEMENTATION")) return 1;
+  if (HDR1("sfx.h", "sfx.o", "SFX_IMPLEMENTATION")) return 1;
+  if (HDR1("snd.h", "snd.o", "SND_IMPLEMENTATION")) return 1;
+  if (HDR1("snk.h", "snk.o", "SNK_IMPLEMENTATION")) return 1;
+  if (HDR1("tmr.h", "tmr.o", "TMR_IMPLEMENTATION")) return 1;
 
   if (link_exe()) return 1;
 
